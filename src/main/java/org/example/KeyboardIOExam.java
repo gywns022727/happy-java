3417cb228157f1828fc42e42099e39aa22d7ebf7
gywns022727 <lifford050227a@gmail.com>
2023-09-15 15:54:22 +0900

    update

---
+++ src/main/java/org/example/KeyboardIOExam.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class KeyboardIOExam {
+    public static void main(String[] args) throws Exception {
+        // 키보드로부터 한 줄씩 입력 받고, 한 줄씩 화면에 출력하시오.
+        // 키보드 : System.in
+        // 화면에 출력 : System.out
+        // 키보드로 입력 받는 다는건 문자를 입력 받는 것 : char 단위 입출력
+        // 한줄 읽기 : BufferedReader라는 클래스는 readLine이라는 메소드를 가지고 있다.
+        // 한줄 쓰기 : PrintStream, PrintWriter
+
+//        31:00
+    }
+}
