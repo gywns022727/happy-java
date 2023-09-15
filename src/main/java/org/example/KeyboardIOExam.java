98e6008ab5a703dea3fa273ea980ecead6f2f0ee
gywns022727 <lifford050227a@gmail.com>
2023-09-15 19:30:00 +0900

    update

--- src/main/java/org/example/KeyboardIOExam.java
+++ src/main/java/org/example/KeyboardIOExam.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
 public class KeyboardIOExam {
     public static void main(String[] args) throws Exception {
         // 키보드로부터 한 줄씩 입력 받고, 한 줄씩 화면에 출력하시오.
@@ -7,8 +11,17 @@ public class KeyboardIOExam {
         // 화면에 출력 : System.out
         // 키보드로 입력 받는 다는건 문자를 입력 받는 것 : char 단위 입출력
         // 한줄 읽기 : BufferedReader라는 클래스는 readLine이라는 메소드를 가지고 있다.
+        // 더이상읽을게 없으면 null을 반환
         // 한줄 쓰기 : PrintStream, PrintWriter
 
-//        31:00
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String line = null;
+        br.readLine(); // 한줄 입력 받는다.
+        while((line = br.readLine()) != null){
+            System.out.println("읽어들인 값 : " + line);
+
+        }
+
     }
 }
